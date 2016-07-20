a08776adb4478b4b9dbc271148cc9c818e1e347a
Alexandr Martynov <alex@magemore.com>
2016-07-20 19:28:39 +0300

    vim edit

--- mage/compile.php
+++ mage/compile.php
@@ -1,4 +1,36 @@
 <?php
 
-echo $argv[1];
+class file {
+  private $path;
+  public function __construct($path) {
+    $this->path = $path;
+  }
+  public function get($path=false) {
+    if ($path) $this->path = $path;
+    $contents = file_get_contents($this->path);
+    $a = explode("\n",$contents);
+    return $a;
+  }
+  public function set($value) {
+    $value = implode("\n",$value);
+    file_put_contents($this->path,$value);
+  }
+}
+
+$file = new file($argv[1]);
+$a = $file->get();
+$commands = array();
+$command_index = -1;
+foreach ($a as $l) {
+  // command mode
+  if ($l[0]!=' ') {
+    $command_index++;
+    $commands[$command_index]['main']=trim($l);
+  }
+  else {
+    $commands[$command_index]['nodes'][]=trim($l);
+  }
+}
+
+print_r($commands);
 echo "\n";

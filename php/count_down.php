5fdd7853f66d8d963d937aa9c0b2feb85cb2c74a
Alexandr Martynov <alex@magemore.com>
2016-07-23 22:43:08 +0300

    xc

--- php/count_down.php
+++ php/count_down.php
@@ -1,3 +1,37 @@
 <?php
 
-print_r($argv);
+if (!isset($argv[1])) {
+  echo 'set minutes in argument';
+  echo "\n";
+  return 0;
+}
+$minutes = $argv[1];
+
+$message='';
+if (isset($argv[2])) $message=$argv[2];
+$now = $start=time();
+$end = $start+$minutes*60;
+
+while ($now<$end) {
+  sleep(1);
+  $f="i:s";
+  if (($end-$now)/360>=1) $f="H:i:s";
+  $m='count down';
+  if ($message) $m=$message;
+  echo $m.' '.gmdate($f,($end-$now))."\n";
+  $now=time();
+}
+
+function say($msg) {
+  echo $msg."\n";
+  system("say '$msg'");
+}
+
+$msg ='count '.$minutes.' minutes ended ';
+if ($message) {
+  $msg=$message.' '.$msg;
+}
+say($msg);
+
+return 1;
+

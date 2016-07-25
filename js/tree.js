cb1a1a96a4edb8f518ca9d62e156e5455f73f361
Alexandr Martynov <alex@magemore.com>
2016-07-25 21:51:45 +0300

    xc

--- js/tree.js
+++ js/tree.js
@@ -9,6 +9,29 @@
 
   f = '/srv/mage2/esf_refine_tree.txt';
 
+  function indentLevel(level) {
+    var s='';
+    var tab='  ';
+    for (var i = 0; i<level; i++) {
+      s+=tab;
+    }
+    return s;
+  }
+
+  function parseList(data,level) {
+    for (var i in data) {
+      for (var j in data[i]) {
+        if (j=='toString') continue;
+        parseList(data[i][j],level+1)
+        c(indentLevel(level)+j);
+      }
+    }
+  }
+
+  function convertToMagentoTree(data) {
+    parseList(data,0);
+  }
+
   fs.readFile(f, 'utf8', function(err, data) {
     var i, j, k, l, len, results, tree;
     if (err) {
@@ -17,21 +40,24 @@
     var data = data.split("\n");
     var tree = tabdown.parse(data);
     var data = tree.root;
-    var results = [];
-    for (var i in data) {
-      for (var category in data[i]) {
-        if (category=='toString') continue;
-        c(category);
-        //c(data[i][category]);
-        for (var j in data[i][category]) {
-          for (var prop in data[i][category][j]) {
-            if (prop=='toString') continue;
-            c('  '+prop);
-          }
-        }
-      }
-    }
-    return results;
-  });
 
+    convertToMagentoTree(data);
+    // for (var i in data) {
+    //   for (var category in data[i]) {
+
+    //     if (category=='toString') continue;
+    //     c(category);
+    //     //c(data[i][category]);
+    //     for (var j in data[i][category]) {
+
+    //       for (var prop in data[i][category][j]) {
+    //         if (prop=='toString') continue;
+    //         c('  '+prop);
+    //       }
+
+    //     }
+    //   }
+    // }
+
+  });
 }).call(this);

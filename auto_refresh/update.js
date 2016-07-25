4b2ff26d71d3a4bf664f3cfba506ed371042ef31
Alexandr Martynov <alex@magemore.com>
2016-07-25 16:26:42 +0300

    xc

--- auto_refresh/update.js
+++ auto_refresh/update.js
@@ -1,39 +1,41 @@
+var auto_refresh = function(){
+  function refresh() {
+      location.reload();
+  }
 
-function refresh() {
-    location.reload();
-}
-
-var last_check = false;
-function check_files_updated() {
-  // console.log('cool');
-
-  //console.log('make request');
+  var last_check = false;
   var request = new XMLHttpRequest();
   request.onreadystatechange = function() {
-    // console.log(request)
-    // console.log(request.status)
-    if (request.status == 200) {
-      var data = request.responseText;
-      console.log(request.responseText);
-      // Success!
-      var data=request.responseText;
-      if (!last_check) {
-        last_check = data;
-        return;
+      // console.log(request)
+      // console.log(request.status)
+      if (request.status == 200) {
+        var data = request.responseText;
+        //console.log(request.responseText);
+        // Success!
+        var data=request.responseText;
+        if (!last_check) {
+          last_check = data;
+          return;
+        }
+        if (data) {
+          if (last_check!=data) {
+              console.log(data);
+              last_check=data;
+            //refresh();
+          }
+        }
+      } else {
+        // We reached our target server, but it returned an error
       }
-      if (last_check!=data) {
-        last_check=data;
-        //refresh();
-      }
-    } else {
-      // We reached our target server, but it returned an error
-    }
-    setTimeout(check_files_updated,500);
   };
-  request.open('GET', '/auto_refresh/update.php', true);
-  request.send();
+
+  function check_files_updated() {
+    request.open('GET', '/auto_refresh/update.php', true);
+    request.send();
+    setTimeout(check_files_updated,1000);
+  }
+
+  check_files_updated();
 }
 
-check_files_updated();
-
-
+auto_refresh()
--- auto_refresh/update.php
+++ auto_refresh/update.php
@@ -1,10 +1,23 @@
 <?php
 
+// todo make it domain specific
+
+// echo '<pre>';
+// print_r($_SERVER);
+// print_r($_SERVER['DOCUMENT_ROOT']);
+// exit();
+
+// @todo: check if works when included from other hosts
+
 require 'short.php';
 
 $c = [];
 # find all files
-$c[] = "find \${1} -type f";
+//if ()
+$c[] = "find $_SERVER[DOCUMENT_ROOT] -type f";
+
+// print_r($c);
+// exit();
 # ignore some
 $c[] = "grep -v '.git' | grep -v '.swp' | grep -v logs | grep -v '-'";
 # limit results to or in file names
@@ -12,7 +25,9 @@ $c[] = "egrep 'html|php|.js|.css'";
 # make it looking good and what is important display date
 $c[] = "xargs stat --format '%Y :%y %n' 2>/dev/null | sort -nr | cut -d: -f2- 2>/dev/null | head";
 $c   = implode(' | ', $c);
-$r   = md5(sr($c));
+// echo $c;
+// exit();
+$r = md5(sr($c));
 
 echo $r;
 echo n;

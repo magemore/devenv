84b2e22cd8e993437ecbd2dfe60049c80a0c033d
Alexandr Martynov <alex@magemore.com>
2016-07-25 22:25:42 +0300

    xc

--- js/tree.js
+++ js/tree.js
@@ -1,67 +1,47 @@
-(function() {
-  var c, f, fs, tabdown;
+var tabdown = require('tabdown');
 
-  tabdown = require('tabdown');
+c = console.log;
 
-  c = console.log;
+var fs = require('fs');
 
-  fs = require('fs');
+var f = '/srv/mage2/esf_refine_tree.txt';
 
-  f = '/srv/mage2/esf_refine_tree.txt';
-
-  function indentLevel(level) {
-    var s='';
-    var tab='  ';
-    for (var i = 0; i<level; i++) {
-      s+=tab;
-    }
-    return s;
+function indentLevel(level) {
+  var s='';
+  var tab='  ';
+  for (var i = 0; i<level; i++) {
+    s+=tab;
   }
+  return s;
+}
 
-  function parseList(data,level) {
-    var t='';
-    for (var i in data) {
-      for (var j in data[i]) {
-        if (j=='toString') continue;
-        t=indentLevel(level);
-        c(t+j);
-        parseList(data[i][j],level+1);
-      }
+function parseList(data,level) {
+  var t='';
+  for (var i in data) {
+    for (var j in data[i]) {
+      if (j=='toString') continue;
+      t=indentLevel(level);
+      c(t+j);
+      parseList(data[i][j],level+1);
     }
   }
+}
 
-  function convertToMagentoTree(data) {
-    parseList(data,0);
-  }
+function convertToMagentoTree(data) {
+  parseList(data,0);
+}
 
 c('start:');
 c('');
-  fs.readFile(f, 'utf8', function(err, data) {
-    var i, j, k, l, len, results, tree;
-    if (err) {
-      c(err);
-    }
-    var data = data.split("\n");
-    var tree = tabdown.parse(data);
-    var data = tree.root;
+fs.readFile(f, 'utf8', function(err, data) {
+  if (err) {
+    c(err);
+  }
+  data = data.split("\n");
+  var tree = tabdown.parse(data);
+  data = tree.root;
 
-    convertToMagentoTree(data);
-    // for (var i in data) {
-    //   for (var category in data[i]) {
+  convertToMagentoTree(data);
 
-    //     if (category=='toString') continue;
-    //     c(category);
-    //     //c(data[i][category]);
-    //     for (var j in data[i][category]) {
 
-    //       for (var prop in data[i][category][j]) {
-    //         if (prop=='toString') continue;
-    //         c('  '+prop);
-    //       }
-
-    //     }
-    //   }
-    // }
-
-  });
-}).call(this);
+});

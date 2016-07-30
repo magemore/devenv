d890ccbaa794ac37dc4929b8607ea03ae953445f
magemore <alex@magemore.com>
2016-07-30 10:20:12 +0300

    Update tools.php

--- webapps/1/tools.php
+++ webapps/1/tools.php
@@ -5,15 +5,21 @@ require_once 'auto_refresh/short.php';
 
 class Exec {
 	
-	private $raw;
+	private $raw = [];
+	private $total = false;
 
-	public function __construct($shellCommand=false) {
-		$this->raw = wrapper_over_exec($shellCommand);
+	public function __construct($shellCommand=false)
+	{
+		$a = wrapper_over_exec($shellCommand);
+		if (strpos(strtolower($a[0]),'total')!==FALSE) {
+			$this->total = $a[0];
+		}
+		$this->$raw=$a;
 		return $this;
 	}
 	
 	public function get() {
-	 	return $this->raw;
+	 	return $this->total;
 	}
 		
 }

974fc7768770abc91fa7fa165a906d814b7008a3
magemore <alex@magemore.com>
2016-08-15 03:19:06 +0300

    M  xc.fish

--- fish/functions/xc.fish
+++ fish/functions/xc.fish
@@ -2,9 +2,10 @@ function xc
     git add .
     if test (count $argv) -gt 0;
         git commit -m (string join ' ' $argv)
+        timep (string join ' ' $argv)
     else
         set STA (git status -s .)
-        git commit -m (string join '; ' $STA)
+        timep (string join '; ' $STA)
     end
     git push origin ^/dev/null >/dev/null &
 end

ee4888a88e5096f0f2c867bc559c81b016256aee
magemore <alex@magemore.com>
2016-08-22 03:29:48 +0300

    M  xc.fish

--- fish/functions/xc.fish
+++ fish/functions/xc.fish
@@ -6,7 +6,7 @@ function xc
     else
         set STA (git status -s .)
         if test (count $STA) -gt 0;
-            timep git commit (pwd) (string join '; ' $STA)
+            timep (pwd) git commit (string join '; ' $STA)
             git commit -m (string join '; ' $STA)
         end
     end

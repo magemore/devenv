cde72e306789d8ee08546674f8b53f761184d5da
magemore <alex@magemore.com>
2016-10-29 23:41:45 +0300

    M  c.fish

--- fish/functions/c.fish
+++ fish/functions/c.fish
@@ -1,14 +1,9 @@
-function ctest
-  echo $argv
-end
-
-
 function c
-  if begin; test (count $argv) -gt 0; and test -d $argv[1]; end
+  if begin; test (count $argv) -gt 0; end
     if begin; test -d $argv[1]; end
       cd $argv[1]
     else
-      ls -lhtra
+      ls -lhtra | grep -i $argv[1]
     end
   else
     clear

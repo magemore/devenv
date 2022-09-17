f517efc5668fe6e94dbc864a5554af47d3b72444
Alexandr Martynov <alex@magemore.com>
2022-09-17 10:26:34 +0300

    fix

--- fish/functions/xc.fish
+++ fish/functions/xc.fish
@@ -3,11 +3,8 @@ function xc
     if test (count $argv) -gt 0;
         git commit -m (string join ' ' $argv)    
     else
-        set STA (git status -s .)
-        if test (count $STA) -gt 0;
-            git commit -m 'fix'
-        end
+        git commit -m 'fix'
     end
     date
-    git push origin ^/dev/null >/dev/null &
+    git push origin
 end

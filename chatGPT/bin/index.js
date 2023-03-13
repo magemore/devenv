e4aeba7d1b44795fcecb03bec8d174494dde7392
Alexandr Martynov <alex@magemore.com>
2023-03-14 00:12:40 +0200

    modified code of terminalGPT to use with fish commands a.fish and chat.fish. a.fish just gives one answer and exists. use: a what is js. chat.fish same way uses cli arguments for first message and then continues chat

---
+++ chatGPT/bin/index.js
@@ -0,0 +1,133 @@
+#!/usr/bin/env node
+
+const commander = require("commander");
+const prompts = require("prompts");
+const chalk = require("chalk");
+const process = require("process");
+
+const { intro } = require("./intro");
+const { apiKeyPrompt, generateResponse } = require("./utils");
+const { deleteApiKey } = require("./encrypt");
+
+let start = true;
+
+commander
+  .command("chat")
+  .option("-e, --engine <engine>", "GPT-3 model to use")
+  .option("-t, --temperature <temperature>", "Response temperature")
+  .option(
+    "-f,--finetunning <finetunning>",
+    "Opt in to pretrain the model with a prompt"
+  )
+  .option("-l,--limit <limit>", "The limit of prompts to train the model with")
+  .usage(`"<project-directory>" [options]`)
+  .action(async () => {
+    intro();
+    apiKeyPrompt().then((apiKey) => {
+      const prompt = async () => {
+        let response = false;
+        if (start && process.argv[3] !== undefined) {          
+          start = false;
+            response = { value: process.argv[3]};
+        }
+        else {
+          response = await prompts({
+            type: "text",
+            name: "value",
+            message: `${chalk.blueBright("You: ")}`,
+            validate: () => {
+              return true;
+            },
+            onState: (state) => {
+              if (state.aborted) {
+                process.exit(0);
+              }
+            }
+          });
+        }
+
+        switch (response.value) {
+          case "exit":
+            return process.exit(0);
+          case "clear":
+            return process.stdout.write("\x1Bc");
+          default:
+            generateResponse(apiKey, prompt, response);
+            return;
+        }
+      };
+
+      prompt();
+    });
+  });
+
+
+  commander
+  .command("answer")
+  .option("-e, --engine <engine>", "GPT-3 model to use")
+  .option("-t, --temperature <temperature>", "Response temperature")
+  .option(
+    "-f,--finetunning <finetunning>",
+    "Opt in to pretrain the model with a prompt"
+  )
+  .option("-l,--limit <limit>", "The limit of prompts to train the model with")
+  .usage(`"<project-directory>" [options]`)
+  .action(async () => {
+    apiKeyPrompt().then((apiKey) => {
+      const prompt = async () => {
+        let response = false;
+        if (start && process.argv[3] !== undefined) {          
+          start = false;
+            response = { value: process.argv[3]};
+        }
+        else {
+          process.exit(0);
+        }
+
+        switch (response.value) {
+          case "exit":
+            return process.exit(0);
+          case "clear":
+            return process.stdout.write("\x1Bc");
+          default:
+            generateResponse(apiKey, prompt, response);
+            return;
+        }
+      };
+      
+      prompt();
+    });
+  });
+
+
+// create commander to delete api key
+
+commander
+  .command("delete")
+  .description("Delete your API key")
+  .action(async () => {
+    const response = await prompts({
+      type: "select",
+      name: "value",
+      message: "Are you sure?",
+      choices: [
+        { title: "Yes", value: "yes" },
+        { title: "No - exit", value: "no" },
+      ],
+      initial: 0,
+    });
+
+    switch (response.value) {
+      case "no":
+        return process.exit(0);
+      case "yes":
+      default:
+        // call the function again
+        deleteApiKey();
+        break;
+    }
+    deleteApiKey();
+    console.log("API key deleted");
+  });
+
+commander.parse(process.argv);
---
+++ chatGPT/bin/utils.js
@@ -0,0 +1,111 @@
+const clipboard = require("clipboardy");
+const { encrypt, saveApiKey, getApiKey } = require("./encrypt");
+const prompts = require("prompts");
+const chalk = require("chalk");
+const process = require("process");
+const { generateCompletion } = require("./gpt");
+
+const apiKeyPrompt = async () => {
+  let apiKey = getApiKey();
+
+  if (!apiKey) {
+    const response = await prompts({
+      type: "password",
+      name: "apiKey",
+      message: "Enter your OpenAI API key:",
+      validate: (value) => {
+        return value !== "";
+      },
+    });
+
+    apiKey = response.apiKey;
+    saveApiKey(encrypt(apiKey));
+  }
+
+  return apiKey;
+};
+
+const checkBlockOfCode = async (text, prompt) => {
+  prompt();
+  return ;
+  // get all matches of text within ```
+  const regex = /```[\s\S]*?```/g;
+  const matches = text.match(regex);
+  if (!matches) {
+    prompt();
+  } else {
+    const recentText = matches[0];
+    const recentTextNoBackticks = recentText.replace(/```/g, "");
+    const response = await prompts({
+      type: "confirm",
+      name: "copy",
+      message: `Copy recent code to clipboard?`,
+      initial: true,
+    });
+
+    if (response.copy) {
+      clipboard.writeSync(recentTextNoBackticks);
+      prompt();
+    } else {
+      prompt();
+    }
+  }
+};
+
+const generateResponse = async (apiKey, prompt, response) => {
+  try {
+    const request = await generateCompletion(
+      apiKey,
+      response.value
+    );
+
+    if (request == undefined || !request?.content) {
+      throw new Error("Something went wrong!");
+    }
+
+    // map all choices to text
+    const getText = [request.content];
+
+    console.log(`${chalk.cyan("GPT-3: ")}`);
+    // console log each character of the text with a delay and then call prompt when it finished
+    let i = 0;
+    const interval = setInterval(() => {
+      if (i < getText[0].length) {
+        process.stdout.write(getText[0][i]);
+        i++;
+      } else {
+        clearInterval(interval);
+        console.log("\n");
+        checkBlockOfCode(getText[0], prompt);
+      }
+    }, 10);
+  } catch (err) {
+    console.error(`${chalk.red("1 Something went wrong!!")} ${err}`);
+    // create a prompt of type select , with the options to exit or try again
+    const response = await prompts({
+      type: "select",
+      name: "value",
+      message: "Try again?",
+      choices: [
+        { title: "Yes", value: "yes" },
+        { title: "No - exit", value: "no" },
+      ],
+      initial: 0,
+    });
+
+    switch (response.value) {
+      case "no":
+        return process.exit(0);
+      case "yes":
+      default:
+        // call the function again
+        generateResponse(apiKey, prompt, options, response);
+        break;
+    }
+  }
+};
+
+module.exports = {
+  apiKeyPrompt,
+  generateResponse,
+};
